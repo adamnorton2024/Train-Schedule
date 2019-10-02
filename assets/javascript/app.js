1e5e4f4bc4aa51ae103f659aba52c38915a86e72
Adam Norton <adam@adamnorton.com>
2019-10-02 15:37:51 -0500

    fixed bugs with calculating and displaying data in table. Only some formatting issues left now.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,13 +16,18 @@ var database = firebase.database();
 $("#add-train-btn").on("click", function (event) {
     event.preventDefault();
 
+    var inputTime = $("#input-first-train-time").val().trim();
+    var hours = inputTime.split(":")[0];
+    var mins = inputTime.split(":")[1];
+
     // Grabs user input to add train
     var trainName = $("#input-train-name").val().trim();
     var trainDestination = $("#input-destination").val().trim();
-    var firstTrainTime = moment($("#input-first-train-time").val().trim(), "MM/DD/YYYY").format("X");
+    var firstTrainTime = moment().hours(hours).minutes(mins).format("X");
     var trainFrequency = $("#input-frequency").val().trim();
     var trainTimeLeft = 0;
 
+    console.log(firstTrainTime);
     // create temp object for holding train data
     var newTrain = {
         name: trainName,
@@ -33,8 +38,8 @@ $("#add-train-btn").on("click", function (event) {
     };
 
     // Uploads employee data to the database
-    database.ref().push(newTrain);
-    console.log(newTrain);
+    database.ref("train").push(newTrain);
+    //console.log(newTrain);
 
     // Clears all of the text-boxes
     $("#input-train-name").val("");
@@ -42,22 +47,12 @@ $("#add-train-btn").on("click", function (event) {
     $("#input-first-train-time").val("");
     $("#input-frequency").val("");
 
-    // // Create the new row
-    // var newRow = $("<tr>").append(
-    //     $("<td>").text(newTrain.name),
-    //     $("<td>").text(newTrain.destination),
-    //     $("<td>").text(newTrain.firstTime),
-    //     $("<td>").text(newTrain.frequency),
-    //     $("<td>").text(newTrain.timeLeft)
-    // );
-
-    
-
 });
-
+    var test;
 // 3. Create Firebase event for adding train
-database.ref().on("child_added", function (childSnapshot) {
+database.ref('train').on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
+    test = childSnapshot.val();
 
     // Store everything into a variable.
     var trainName = childSnapshot.val().name;
@@ -73,16 +68,37 @@ database.ref().on("child_added", function (childSnapshot) {
     console.log(frequency);
     console.log(trainTimeLeft);
 
-    // Prettify the employee start
-    var trainStartPretty = moment.unix(firstTrainTime).format("HH:mm");
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+    //console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    //console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    //console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    //console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = frequency - tRemainder;
+    //console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    //console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
     // Create the new row
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(trainDestination),
-        $("<td>").text(trainStartPretty),
+        $("<td>").text(firstTrainTime),
         $("<td>").text(frequency),
-        $("<td>").text(trainTimeLeft)
+        $("<td>").text(tMinutesTillTrain)
     );
 
     // Append the new row to the table
